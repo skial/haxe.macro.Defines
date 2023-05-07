bb0132a584f2158d075aeaa41cc2b542c345ebb5
skial <skial.bainn@gmail.com>
2023-05-07 13:20:15 +0100

    Make sure the string print out as you expect.

--- tests/Main.hx
+++ tests/Main.hx
@@ -5,10 +5,10 @@ using haxe.macro.Defines;
 class Main {
 
     public static function main() {
-        trace( Defines.AbsolutePath.get() == null );
-        trace( Dce.get() == DceValues.Std );
-        trace( SwfCompressLevel.get() == SwfCompressLevelRange.Level7 );
-        trace( Eval == true );
+        trace( Defines.AbsolutePath.get() == null, AbsolutePath );
+        trace( Dce.get() == DceValues.Std, Dce, Std );
+        trace( SwfCompressLevel.get() == SwfCompressLevelRange.Level7, SwfCompressLevel, Level7 );
+        trace( Eval == true, Eval );
     }
 
 }
