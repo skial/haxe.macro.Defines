76b99ae8edc7e0fc3194e35b8e2a1f9a0bd736a3
skial <skial.bainn@gmail.com>
2025-01-20 14:13:12 +0000

    Update to Haxe 5 nightly 6ab9980 Removes all c# and java defines. Adds a lot of HXCPP defines. Fix "simple" tests/Main.hx

--- tests/Main.hx
+++ tests/Main.hx
@@ -6,7 +6,7 @@ class Main {
 
     public static function main() {
         trace( Defines.AbsolutePath.get() == null, AbsolutePath );
-        trace( Dce.get() == DceValues.Std, Dce, Std );
+        trace( Dce.get() == DceValues.Standard, Dce, Standard );
         trace( SwfCompressLevel.get() == SwfCompressLevelRange.Level7, SwfCompressLevel, Level7 );
         trace( Eval == true, Eval );
     }
